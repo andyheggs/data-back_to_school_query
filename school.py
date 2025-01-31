bf2e6df746729d5ee0ddfd31d0bb6b75d854ce18
andy heggs <andy@heggs.net>
2025-01-31 13:56:05 +0000

    SQL_Advanced-Warm_up-_2nd_Commit

--- school.py
+++ school.py
@@ -2,14 +2,23 @@
 
 def students_from_city(db, city):
     """return a list of students from a specific city"""
-    pass  # YOUR CODE HERE
+
+    # Define query
+    query = "SELECT * FROM students AS student WHERE birth_city = ?"
+
+    # Execute query w specified city
+    db.execute(query, (city,))
+
+    # Fetch results
+    results = db.fetchall()
+
+    # Close con
+    conn.close()
+
+    return results
 
 
-# To test your code, you can **run it** before running `make`
-#   => Uncomment the following lines + run:
-#   $ python school.py
-#
-# import sqlite3
-# conn = sqlite3.connect('data/school.sqlite')
-# db = conn.cursor()
-# print(students_from_city(db, 'Paris'))
+import sqlite3
+conn = sqlite3.connect('data/school.sqlite')
+db = conn.cursor()
+print(students_from_city(db, 'Paris'))
